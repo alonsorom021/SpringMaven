e006bd3f590b0c5e5b82158cf5249d9e76d5949a
Ing. Ernesto Alonso Romero Muñoz <aloromerooya280@gmail.com>
2023-06-13 14:46:40 -0600

    SpringMaven

---
+++ src/test/java/com/persona/SpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
